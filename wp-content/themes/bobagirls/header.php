e4b48696483b0cbc0485c87e07c4943a3d658d1c
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2023-04-13 17:30:29 -0400

    en/fr menu working to switch languagel

--- wp-content/themes/bobagirls/header.php
+++ wp-content/themes/bobagirls/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -23,48 +25,54 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+	<?php wp_body_open(); ?>
 
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bobagirls' ); ?></a>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'bobagirls'); ?></a>
 
-	<header id="masthead" class="site-header">
+		<header id="masthead" class="site-header">
 
-	<div class="header-inner container">
-		<div class="site-branding">
-				<!-- here we link to our logo from the assets folder -->
-				<img src="<?php bloginfo('template_url'); ?>/assets/cat.jpg" class="logo">
-			<!-- <?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?> 
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<div class="header-inner container">
+				<div class="site-branding">
+					<!-- here we link to our logo from the assets folder -->
+					<img src="<?php bloginfo('template_url'); ?>/assets/cat.jpg" class="logo">
+					<!-- <?php
+							the_custom_logo();
+							if (is_front_page() && is_home()) :
+							?> 
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 				<?php
-			else :
+							else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
 				<?php
-			endif;
-			$bobagirls_description = get_bloginfo( 'description', 'display' );
-			if ( $bobagirls_description || is_customize_preview() ) :
+							endif;
+							$bobagirls_description = get_bloginfo('description', 'display');
+							if ($bobagirls_description || is_customize_preview()) :
 				?>
-				<p class="site-description"><?php echo $bobagirls_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<p class="site-description"><?php echo $bobagirls_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+											?></p>
 			<?php endif; ?> -->
-		</div><!-- .site-branding -->
+				</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-		<!-- todo: make this atually work -->
-		<div class="language-select">EN | FR</div>
+				<nav id="site-navigation" class="main-navigation">
 
-		</div>
-	</header><!-- #masthead -->
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				</nav><!-- #site-navigation -->
+				<!-- todo: make this atually work -->
+				<div class="language-select">
+					<a lang="en=US" hreflang="en=US" href="<?php echo site_url('/en'); ?>">EN</a>
+					|
+					<a lang="fr=FR" hreflang="fr=FR" href="<?php echo site_url('/kr'); ?>">FR</a>
+
+				</div>
+
+			</div>
+		</header><!-- #masthead -->
